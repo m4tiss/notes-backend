4795ba652ca478a8fbaa45903d897aa01b635ce2
m4tiss <mateusz039@op.pl>
2024-02-25 14:13:13 +0100

    final version 1.2

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const saveNote = require('../actions/saveNote.js');
+const saveNote = require('../actions/savenote.js');
 const getNote = require('../actions/getNote.js');
 const deleteNote = require('../actions/deleteNote.js');
 const editNote = require('../actions/editNote.js');
